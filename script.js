56705e0d93b6b7f7af1f40f7c6439e52cbd0cbaf
Moe Doi <35331371+soilimoe@users.noreply.github.com>
2026-06-01 17:56:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-document.getElementById("quiz").innerHTML =
-"GitHub Pages成功！";
+const csvUrl = "https://docs.google.com/spreadsheets/d/1pcYtP8XjDBDOrn5T90sLkeIQGJSOHEAisLU7m0QnPHg/gviz/tq?tqx=out:csv&sheet=生理・生化";
+
+fetch(csvUrl)
+  .then(response => response.text())
+  .then(data => {
+    console.log(data);
+
+    document.getElementById("quiz").innerHTML =
+      "<pre>" + data + "</pre>";
+  });
