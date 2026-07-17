0bf2fd5eb8f4d6dc897e35181ec51e68b0098f55
Moe Doi <35331371+soilimoe@users.noreply.github.com>
2026-07-17 18:34:20 +0900

    Update script.js マイナーチェンジ

--- script.js
+++ script.js
@@ -440,7 +440,7 @@ function saveScore(){
             window.answerCount
         );
 
-    fetch(GAS_URL,{
+    fetch("https://script.google.com/macros/s/AKfycbwtMUofKXcuujajuE85yP2eFjPrP_w5WSqAkpJzrXX3x581oeFCtAhFsCe4l7Wr49AWlg/exec",{
 
         method:"POST",
 
